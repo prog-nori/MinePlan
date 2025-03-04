17c1f08f5141334f1696ff60c57d753d0ecc8432
prog-nori <norinorimcz@gmail.com>
2025-03-04 23:15:16 +0900

    add deploy settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.base_URL || '/',
+  },
+  nitro: {
+    prerender: {
+      routes: ['/'],
+    },
+  },
   rootDir: './app',
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
